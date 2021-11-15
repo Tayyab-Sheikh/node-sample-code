ee4b1c145bd15c2085fcbeb514c6977a57381435
Harry <tayabsheikh900@gmail.com>
2021-11-15 11:49:18 +0500

    changes routes to node-sample project

--- api/routers/system.routers.js
+++ api/routers/system.routers.js
@@ -4,6 +4,7 @@ const express = require(`express`);
 //importing middlewares
 const {authenticateRequest} = require(`../middlewares/authentication.middleware`);
 const {validateInput} = require(`../middlewares/input-validation.middleware`);
+const {authorizeRequest} = require(`../middlewares/authorization.middleware`);
 
 //importing require dataValid 
 const {specificSUserSchema, specificProductSchema} = require(`../../dependencies/input-validation/user.schema`);
@@ -14,18 +15,18 @@ const {addProduct, getAllProducts, findProductById, findProductByIdAndUpdate , a
 const systemRouter = express.Router();
 
 // 1-> route to add a new product in the database
-systemRouter.post(`/product`, authenticateRequest, validateInput(`BODY`), addProduct);
+systemRouter.post(`/product`, authenticateRequest, authorizeRequest, validateInput(`BODY`), addProduct);
 
 // 1-> route to fetch a specific product from database via _id
 // 2-> route to fetch all products as an array from database
-systemRouter.get(`/product:productId`, authenticateRequest, validateInput(specificProductSchema, `PARAMS`),findProductById);
-systemRouter.get(`/products`, authenticateRequest, validateInput(`NONE`), getAllProducts);
+systemRouter.get(`/product:productId`, authenticateRequest, authorizeRequest, validateInput(specificProductSchema, `PARAMS`),findProductById);
+systemRouter.get(`/products`, authenticateRequest, authorizeRequest, validateInput(`NONE`), getAllProducts);
 
 // 1-> route to update a specific product in the database via _id
-systemRouter.patch(`/product:productId`, authenticateRequest, validateInput(specificProductSchema, `PARAMS`), validateInput(`BODY`), findProductByIdAndUpdate);
+systemRouter.patch(`/product:productId`, authenticateRequest, authorizeRequest, validateInput(specificProductSchema, `PARAMS`), validateInput(`BODY`), findProductByIdAndUpdate);
 
 // 1-> route to delete a specific product from database via _id
-//systemRouter.delete(`/product:productId`, authenticateRequest, validateInput(specificProductSchema, `PARAMS`), deleteSystemRole);
+//systemRouter.delete(`/product:productId`, authenticateRequest, authorizeRequest, validateInput(specificProductSchema, `PARAMS`), deleteSystemRole);
 
 
 // exporting router as a module
